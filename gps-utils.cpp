d7e5f1822d58dc31cd706f49e2b20ba075e09770
Jacob Frank <jacobfrank4@gmail.com>
2016-10-15 03:31:06 -0700

    continued seg-fault testing

--- gps-utils.cpp
+++ gps-utils.cpp
@@ -19,25 +19,25 @@ void startThread(gpsmm& gps) {
 void readGPS(gpsmm gps) {
     cout << "Before for loop" << endl;
     for (;;) {
-        cout << "before GPS struct creation" << endl;
+        //cout << "before GPS struct creation" << endl;
         struct gps_data_t* newdata;
-        cout << "After GPS struct creation" << endl;
+       // cout << "After GPS struct creation" << endl;
 
-        cout << "Before waiting statement" << endl;
-        if (!gps.waiting(50000000)) {
-            cout << "inside waiting statement" << endl;
-            continue;
-        }
-        cout << "After waiting statement" << endl;
+       // cout << "Before waiting statement" << endl;
+       // if (!gps.waiting(50000000)) {
+       //     cout << "inside waiting statement" << endl;
+       //     continue;
+        //}
+        //cout << "After waiting statement" << endl;
 
         cout << "Before read statement" << endl;
         if ((newdata = gps.read()) == NULL) { //SEG FAULTS HERE***********
             cerr << "Read error.\n";
-          //  return;
-        } //else {
-          //  cout << "inside readGPS" << endl;
-          //  print();
-        //}
+            return;
+        } else {
+            cout << "inside readGPS" << endl;
+            print();
+        }
         cout << "After read statement" << endl;
     }
 }
