4cca9eb243598150ac58f2f23b9085102bceb72d
JDePourcq1 <jdepourcq3@my.bcit.ca>
2016-10-16 11:16:44 -0700

    Update gpsprint.cpp
    basic obtaining of info

--- gpsprint.cpp
+++ gpsprint.cpp
@@ -1,1 +1,45 @@
 //gpsprint.cpp
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gps.h>
+using namespace std;
+
+void time();
+void location();
+void information();
+
+int main(void) {
+    time();
+}
+void time() {
+    
+    struct gps_data_t gpsdata;
+	
+    time_t gpsTime;
+    gpsTime = (time_t) gpsdata.fix.time;
+    cout << gmtime(&gpsTime) << endl;
+}
+
+void location() {
+    
+    struct gps_data_t gpsdata;
+    double lat;
+    double lon;
+    
+    lat = gpsdata.fix.latitude;
+    lon = gpsdata.fix.latitude;
+}
+
+void information() {
+    
+    struct gps_data_t gpsdata;
+    bool use;
+    short prn;
+    short elevate;
+    short azimuth;
+    double SNR;
+    
+    elevate = gpsdata.azimuth;
+}
