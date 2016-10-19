07242fe07503a5abf46aaaa2dc8a9deb780a7550
JDePourcq1 <jdepourcq3@my.bcit.ca>
2016-10-18 19:43:12 -0700

    Update gps-utils.cpp

--- gps-utils.cpp
+++ gps-utils.cpp
@@ -11,49 +11,49 @@ void print();
 void print_gps_data(struct gps_data_t *gpsdata);
 
 /*------------------------------------------------------------------------------------------------------------------
--- FUNCTION: readGPS
---
--- DATE: October 16, 2016
---
--- REVISIONS: (Date and Description)
---
--- DESIGNER: Jacob Frank and Justen DePourq
---
--- PROGRAMMER: Jacob Frank
---
--- RETURNS: void.
---
--- NOTES:
--- function reads data from the gps and passes it to a secondary print function.
--- If function cannot read a GPS signal it will timeout after 250 attempts and exit the program.
-----------------------------------------------------------------------------------------------------------------------*/
+ -- FUNCTION: readGPS
+ --
+ -- DATE: October 16, 2016
+ --
+ -- REVISIONS: (Date and Description)
+ --
+ -- DESIGNER: Jacob Frank and Justen DePourq
+ --
+ -- PROGRAMMER: Jacob Frank
+ --
+ -- RETURNS: void.
+ --
+ -- NOTES:
+ -- function reads data from the gps and passes it to a secondary print function.
+ -- If function cannot read a GPS signal it will timeout after 250 attempts and exit the program.
+ ----------------------------------------------------------------------------------------------------------------------*/
 void readGPS(gpsmm gpsStruct) {
     int timeoutCounter = 0;
-
+    
     for (;;) {
         struct gps_data_t* gpsData;
-
-	//If the function cannot detect a gps signal after 50 milliseconds
-	//the timout counter is incremented and GPS detection is tried again
-	//Can occur a maximum of 250 times before timing out and exiting program
+        
+        //If the function cannot detect a gps signal after 50 milliseconds
+        //the timout counter is incremented and GPS detection is tried again
+        //Can occur a maximum of 250 times before timing out and exiting program
         if (!gpsStruct.waiting(500000)) {
-	    cout << "Timeout++" << endl;
             timeoutCounter++;
-
-            if(timeoutCounter > 250) {
-                cout<< "GPS timeout" << endl;
-		break;
+            
+            if(timeoutCounter % 40 == 0) {
+                cout << "Looking for GPS..." << endl;
             }
-	    continue;
+            
+            if(timeoutCounter > 250) {
+                break;
+            }
+            continue;
         }
-	cout << "before if" << endl;
-	//Ensures the GPS struct is not null before passing it to the print function.
+        //Ensures the GPS struct is not null before passing it to the print function.
         if ((gpsData = gpsStruct.read()) == nullptr) {
-		cout << "break" << endl;
-	    	break;
+            break;
         } else {
-	    cout << "before print call" << endl;
-	    print_gps_data(gpsData);
+            cout << "before print call" << endl;
+            print_gps_data(gpsData);
         }
     }
 }
